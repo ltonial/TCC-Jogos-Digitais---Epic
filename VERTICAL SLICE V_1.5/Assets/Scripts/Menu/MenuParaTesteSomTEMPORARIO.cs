2288bd5b44a2c61b86b58f121cb525d557e43a8a
unknown <Lucas@Lucas-PC.(none)>
2012-06-28 21:07:04 -0300

    Inicio da adição de sons. Primeiro teste com música tema do menu.

---
+++ VERTICAL SLICE V_1.5/Assets/Scripts/Menu/MenuParaTesteSomTEMPORARIO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class MenuParaTesteSomTEMPORARIO : MonoBehaviour {
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            Application.LoadLevel("Game");
+
+        }
+
+    }
+}
